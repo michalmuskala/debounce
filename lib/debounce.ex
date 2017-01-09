fb74c7a06695c7597188b627a8ea9220b30e959b
Michal Muskala <michal@muskala.eu>
2017-01-10 00:40:19 +0100

    Add docs and flush

--- lib/debounce.ex
+++ lib/debounce.ex
@@ -1,52 +1,156 @@
 defmodule Debounce do
+  @moduledoc """
+  A process-based debouncer for Elixir.
+
+  ## What is a debouncer?
+
+  A debouncer is responsible for calling a function with a delay, but if that
+  function is called multiple times within the delay period, the time is reset
+  and delay is counted again. In other words, the function will be called
+  after a delay period has elapsed from the last application.
+
+  Each time, the debounced function is called, a new task is started.
+
+  ## Example
+
+      iex> {:ok, pid} = Debounce.start_link({Kernel, :send, [self(), "Hello"]}, 100)
+      iex> Debounce.apply(pid)  # Schedules call in 100 ms
+      iex> :timer.sleep(50)
+      iex> Debounce.apply(pid)  # Resets timer back to 100 ms
+      iex> :timer.sleep(100)
+      iex> receive do msg -> msg end
+      "Hello"                   # Timer elapsed
+      iex> Debounce.apply(pid)  # Schedules call in 100 ms
+      iex> Debounce.cancel(pid) # Cancels scheduled call
+      :ok
+
+  """
+
   @behaviour :gen_statem
 
+  @type mfargs :: {module, atom, [term]}
+  @type apply :: (() -> term) | mfargs
+  @type time :: non_neg_integer
+  @type debouncer :: :gen_statem.server_ref
+  @type option :: {:name, GenServer.name} | :gen_statem.start_opt
+
+  defmacrop is_apply(apply) do
+    quote do
+      is_function(unquote(apply)) or
+        (is_atom(elem(unquote(apply), 0)) and
+          is_atom(elem(unquote(apply), 1)) and
+          is_list(elem(unquote(apply), 2)))
+    end
+  end
+
+  @doc """
+  Starts a `Debounce` process linked to the current process.
+
+  This can be used to start the `Debounce` as part of a supervision tree.
+
+  Delays invoking `apply` until after `timeout` millisecnds have elapsed
+  since the last time the `apply/2` function was called.
+
+  ## Options
+
+    * `:name`- used for name registration, like in `GenServer.start_link/3`.
+    * all other options supported by `:gen_statem.start_link/4`
+
+  """
+  @spec start_link(apply, time, [option]) :: :gen_statem.start_ret
   def start_link(apply, timeout, opts \\ []) do
     do_start(:start_link, apply, timeout, opts)
   end
 
+  @doc """
+  Starts a `Debounce` process without links (outside of a supervision tree).
+
+  See `start_link/3` for more information.
+  """
+  @spec start(apply, time, [option]) :: :gen_statem.start_ret
   def start(apply, timeout, opts \\ []) do
     do_start(:start, apply, timeout, opts)
   end
 
-  def stop(debounce, reason \\ :normal, timeout \\ :infinity) do
-    :gen_statem.stop(debounce, reason, timeout)
+  @doc """
+  Synchronously stops the debouncer with the given `reason`.
+  """
+  @spec stop(debouncer, reason :: term, timeout) :: :ok
+  def stop(debouncer, reason \\ :normal, timeout \\ :infinity) do
+    :gen_statem.stop(debouncer, reason, timeout)
   end
 
-  def apply(debounce) do
-    :gen_statem.call(debounce, :apply)
+  @doc """
+  Schedules call to the current `debouncer`'s function.
+
+  If the function is a fun, calls it with provided `args`.
+  If the function is an `t:mfargs` appends provided `args` to the original ones.
+
+  If this function is called again withing the current `debouncer`'s timeout
+  value, the time will reset.
+  """
+  @spec apply(debouncer, [term]) :: :ok
+  def apply(debouncer, args \\ []) do
+    call(debouncer, {:apply, args})
   end
 
-  def cancel(debounce) do
-    :gen_statem.call(debounce, :cancel)
+  @doc """
+  Cancels any scheduled call to the current `debouncer`'s function.
+  """
+  @spec cancel(debouncer) :: :ok
+  def cancel(debouncer) do
+    call(debouncer, :cancel)
   end
 
-  def change_function(debounce, new_function) do
-    :gen_statem.call(debounce, {:change_function, mfa(new_function)})
+  @doc """
+  Immediately invokes the current `debouncer`'s function.
+
+  If the function is a fun, calls it with provided `args`.
+  If the function is an `t:mfargs` appends provided `args` to the original ones.
+  """
+  @spec flush(debouncer, [term]) :: :ok
+  def flush(debouncer, args) do
+    call(debouncer, {:flush, args})
   end
 
-  def change_timeout(debounce, new_timeout) when is_integer(new_timeout) do
-    :gen_statem.call(debounce, {:change_timeout, new_timeout})
+  @doc """
+  Changes the function the `debouncer` is applying.
+
+  Affects only future calls to `apply/2`.
+  """
+  @spec change_function(debouncer, apply) :: :ok
+  def change_function(debouncer, new_function) do
+    call(debouncer, {:change_function, new_function})
+  end
+
+  @doc """
+  Changes the delay the `debouncer` operates with.
+
+  Affects only future calls to `apply/2`.
+  """
+  @spec change_timeout(debouncer, time) :: :ok
+  def change_timeout(debouncer, new_timeout) when is_integer(new_timeout) do
+    call(debouncer, {:change_timeout, new_timeout})
   end
 
   ## Callbacks
 
   import Record
 
-  defrecord :data, [:mfa, :timeout]
+  defrecord :data, [:apply, :timeout]
 
   @doc false
   def callback_mode, do: :state_functions
 
   @doc false
-  def init({mfa, timeout}) do
-    {:ok, :waiting, data(mfa: mfa, timeout: timeout)}
+  def init({apply, timeout}) do
+    {:ok, :waiting, data(apply: apply, timeout: timeout)}
   end
 
   @doc false
-  def waiting({:call, from}, :apply, data(mfa: mfa, timeout: timeout) = data) do
+  def waiting({:call, from}, {:apply, args}, data(apply: apply, timeout: timeout) = data) do
     {:next_state, :counting, data,
-     [{:reply, from, :ok}, {:state_timeout, timeout, mfa}]}
+     [{:reply, from, :ok}, {:state_timeout, timeout, {apply, args}}]}
   end
 
   def waiting({:call, from}, :cancel, data) do
@@ -58,17 +162,17 @@ defmodule Debounce do
   end
 
   @doc false
-  def counting({:call, from}, :apply, data(mfa: mfa, timeout: timeout) = data) do
+  def counting({:call, from}, {:apply, args}, data(apply: apply, timeout: timeout) = data) do
     {:keep_state, data,
-     [{:reply, from, :ok}, {:state_timeout, timeout, mfa}]}
+     [{:reply, from, :ok}, {:state_timeout, timeout, {apply, args}}]}
   end
 
   def counting({:call, from}, :cancel, data) do
-    {:next_state, :wating, data, {:reply, from, :ok}}
+    {:next_state, :waiting, data, {:reply, from, :ok}}
   end
 
-  def counting(:state_timeout, {m, f, a}, data) do
-    Task.Supervisor.start_child(Debounce.Supervisor, m, f, a)
+  def counting(:state_timeout, {apply, args}, data) do
+    apply_function(apply, args)
     {:next_state, :waiting, data}
   end
 
@@ -76,24 +180,28 @@ defmodule Debounce do
     handle_event(event, event_content, data)
   end
 
-  @doc false
-  def handle_event({:call, from}, {:change_function, mfa}, data) do
-    {:keep_state, data(data, mfa: mfa), {:reply, from, :ok}}
+  defp handle_event({:call, from}, {:change_function, apply}, data) do
+    {:keep_state, data(data, apply: apply), {:reply, from, :ok}}
   end
 
-  def handle_event({:call, from}, {:change_timeout, timeout}, data) do
+  defp handle_event({:call, from}, {:change_timeout, timeout}, data) do
     {:keep_state, data(data, timeout: timeout), {:reply, from, :ok}}
   end
 
-  def handle_event({:call, _from}, msg, data) do
+  defp handle_event({:call, from}, {:flush, args}, data(apply: apply) = data) do
+    apply_function(apply, args)
+    {:next_state, :waiting, data, {:reply, from, :ok}}
+  end
+
+  defp handle_event({:call, _from}, msg, data) do
     {:stop, {:bad_call, msg}, data}
   end
 
-  def handle_event(:cast, msg, data) do
+  defp handle_event(:cast, msg, data) do
     {:stop, {:bad_cast, msg}, data}
   end
 
-  def handle_event(:info, msg, data) do
+  defp handle_event(:info, msg, data) do
     proc =
       case Process.info(self(), :registered_name) do
         {_, []}   -> self()
@@ -117,12 +225,11 @@ defmodule Debounce do
 
   ## Helpers
 
-  defp do_start(start, apply, timeout, opts) when is_integer(timeout) do
-    mfa = mfa(apply)
+  defp do_start(start, apply, timeout, opts) when is_apply(apply) and is_integer(timeout) do
     if name = name(opts[:name]) do
-      apply(:gen_statem, start, [name, __MODULE__, {mfa, timeout}, opts])
+      apply(:gen_statem, start, [name, __MODULE__, {apply, timeout}, opts])
     else
-      apply(:gen_statem, start, [__MODULE__, {mfa, timeout}, opts])
+      apply(:gen_statem, start, [__MODULE__, {apply, timeout}, opts])
     end
   end
 
@@ -130,14 +237,14 @@ defmodule Debounce do
   defp name(atom) when is_atom(atom), do: atom
   defp name(other), do: other
 
-  defmacrop is_mfa(tuple) do
-    quote do
-      is_atom(elem(unquote(tuple), 0)) and
-      is_atom(elem(unquote(tuple), 1)) and
-      is_list(elem(unquote(tuple), 2))
-    end
+  defp call(debounce, request) do
+    :gen_statem.call(debounce, request, {:dirty_timeout, 5_000})
   end
 
-  defp mfa(fun) when is_function(fun), do: {:erlang, :apply, [fun]}
-  defp mfa(mfa) when is_mfa(mfa), do: mfa
+  defp apply_function({m, f, a}, args) do
+    Task.Supervisor.start_child(Debounce.Supervisor, m, f, a ++ args)
+  end
+  defp apply_function(fun, args) do
+    Task.Supervisor.start_child(Debounce.Supervisor, :erlang, :apply, [fun, args])
+  end
 end
--- test/debounce_test.exs
+++ test/debounce_test.exs
@@ -2,10 +2,35 @@ defmodule DebounceTest do
   use ExUnit.Case
   doctest Debounce
 
-  test "single apply" do
+  test "single apply with mfargs" do
     {:ok, pid} = Debounce.start_link({:erlang, :send, [self(), :done]}, 10)
     refute_receive :done, 15
     assert :ok = Debounce.apply(pid)
+    assert_receive :done, 15
+    refute_receive :done
+  end
+
+  test "single apply with fun" do
+    test = self()
+    {:ok, pid} = Debounce.start_link(fn -> send(test, :done) end, 10)
+    refute_receive :done, 15
+    assert :ok = Debounce.apply(pid)
+    assert_receive :done, 15
+    refute_receive :done
+  end
+
+  test "single apply with mfargs passing arguments" do
+    {:ok, pid} = Debounce.start_link({:erlang, :send, [self()]}, 10)
+    refute_receive :done, 15
+    assert :ok = Debounce.apply(pid, [:done])
+    assert_receive :done, 15
+    refute_receive :done
+  end
+
+  test "single apply with fun passing arguments" do
+    {:ok, pid} = Debounce.start_link(fn to -> send(to, :done) end, 10)
+    refute_receive :done, 15
+    assert :ok = Debounce.apply(pid, [self()])
     assert_receive :done, 15
     refute_receive :done
   end
@@ -67,5 +92,19 @@ defmodule DebounceTest do
     assert :ok = Debounce.apply(pid)
     refute_receive :done, 15
     assert_receive :done, 100
+  end
+
+  test "flush without apply" do
+    {:ok, pid} = Debounce.start_link(fn to -> send(to, :done) end, 10)
+    assert :ok = Debounce.flush(pid, [self()])
+    assert_received :done
+  end
+
+  test "flush after apply" do
+    {:ok, pid} = Debounce.start_link({:erlang, :send, [self()]}, 10)
+    assert :ok = Debounce.apply(pid, [:not_done])
+    assert :ok = Debounce.flush(pid, [:done])
+    assert_received :done
+    refute_receive :not_done
   end
 end
